78a779e5ceaeb2652c721496402750deeb3d7393
Erik Nguyen <loc.nguyent@s3corp.com.vn>
2026-03-11 17:57:28 +0700

    add more package for app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require('helmet')
+const compression = require('compression')
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet())
+app.use(compression())
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "his",
+  });
+});
+
+module.exports = app;
