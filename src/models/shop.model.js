d0b4cb933d2932a26b44bf5ce1daf97199047c29
better_me <erikkhan2019@gmail.com>
2026-03-13 21:53:31 +0700

    init mongo schema (model)

---
+++ src/models/shop.model.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const shopSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      maxLength: 150,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "inactive",
+    },
+    verify: {
+      type: Schema.Types.Boolean,
+      default: false,
+    },
+    roles: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    collection: "Shops",
+  },
+);
+
+module.exports = model("Shop", shopSchema);
